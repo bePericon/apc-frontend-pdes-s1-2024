202dcd6053c1a8e7d1c85d0acb5ac10a1cd67cb5
Brian Pericon <brian.pericon@fravega.com.ar>
2024-04-08 15:25:16 -0300

    Added library MUI

---
+++ frontend/src/components/pages/Login/Login.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import { StyledContainer } from "./Login.styled";
+
+const Login = () => {
+  return (
+    <StyledContainer>
+      
+      <Button variant="contained">Login</Button>
+    </StyledContainer>
+  );
+};
+
+export default Login;
