08686ca623236b7bac1f2f82b807ce1aa8990fc5
Emmanuel Pericon <brianpericon.e92@gmail.com.ar>
2024-05-29 22:26:39 -0300

    Fixed loader

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -8,6 +8,7 @@ import storage from 'redux-persist/lib/storage' // defaults to localStorage for 
 const persistConfig = {
   key: 'root',
   storage,
+  blacklist: ['loader']
 }
 
 const reducer = combineReducers({
